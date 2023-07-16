b45b6a623272f0d2d28de476448437b15a4d07dc
Lucas Torres <109477475+Yoichiroo@users.noreply.github.com>
2023-07-15 22:47:49 -0300

    Funções para o tempo das imgs

--- carousel/js/script.js
+++ carousel/js/script.js
@@ -1,3 +1,13 @@
 let radio = document.querySelector('.btn-manual')
 
 document.getElementById("r1").checked = true
+
+setInterval(
+    () => {
+        proxImg()
+    }
+, 2000)
+
+function proxImg() {
+    
+}
